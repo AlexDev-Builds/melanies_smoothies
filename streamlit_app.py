04d2f3364ef27c022fc6bb418a8306cc9589f06e
AlexDev-Builds <apjackowski@gmail.com>
2026-06-23 17:28:07 +0100

    Testing in fruit chosen

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,6 +29,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     # st.write(ingredients_string)
 
@@ -44,7 +46,5 @@ if ingredients_list:
         
         st.success(icon="✅", body=f'Your Smoothie is ordered, {name_on_order}!')
 
-# New section to display smoothiefroot nutrition information
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-# st.text(smoothiefroot_response.json())
 sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
